0c153bbcaecde8f647c5f1c7d94918c8b4dbfeec
Thilakv117 <thilakv117@gmail.com>
2023-09-19 12:03:11 +0530

    first commit

--- email_slicer.py
+++ email_slicer.py
@@ -1,15 +1,30 @@
-import qrcode
-def generate_code(text):
-    qr = qrcode.QRCode(
-        version = 1,
-        error_correction = qrcode.constants.ERROR_CORRECT_L,
-        box_size= 10,
-        border = 4,
-    )
+def add(task , option ):
+    if option == 'Add'.lower():
+        user_task = input("Enter the Task: ")
+        task.append(user_task)
+        return task
+def task_remove(task, option):
+    if option == 'remove'.lower():
+        user_task = input("Enter the Task to Remove: ")
+        task.remove(user_task)
+        return task
+def leave(option):
+    if option == 'quit':
+        return quit()
 
-    qr.add_data(text)
-    qr.make(fit = True)
-    img = qr.make_image(fill_color = "black", back_color = "white")
-    img.save("qrimg1.png")
-url = input("ENter the URL: ")
-generate_code(url)
+def main():
+    task = []
+    quit = True
+    while quit:
+        print("Add - Adding Task")
+        print("Remove - To Remove the task")
+        print("quit - To Quit")
+        print("")
+        user_option = input("Enter the option: ").lower()
+        add(task, user_option)
+        task_remove(task , user_option)
+        leave(user_option)
+        print("")
+        print(task)
+        print("")
+main()
